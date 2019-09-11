37e697f6c948fca078b2bcb2b0c189bea41092af
edvinb123 <edvin.blomgren@elev.ga.lbs.se>
2019-09-11 10:56:44 +0200

    fix

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject player;
+    public Rigidbody rb;
+    public bool hasLost;
+    public float yta;
+    public bool hasStarted;
+    public List<GameObject> obstacles;
+    public GameObject upperPipe;
+    public GameObject lowerPipe;
+    public float time;
+    public AudioSource sound;
+    public Text pointsText;
+    public float pointsAmount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yta = gameObject.transform.GetSiblingIndex();
+        time = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText.text = "Score: " + pointsAmount.ToString("0.0") + "m";
+        yta += time;
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            GameObject typ = new GameObject();
+            int r = Random.Range(0, 2);
+            if (r == 0)
+            {
+                typ = Instantiate(upperPipe, new Vector3(3, 2.5f, 0), Quaternion.identity);
+            }
+            if (r == 1)
+            {
+                yta = transform.position.y;
+                typ = Instantiate(lowerPipe, new Vector3(3, 0.6f, 0), Quaternion.identity);
+            }
+            obstacles.Add(typ);
+            time = 3;
+        }
+
+        if (hasStarted)
+        {
+            if (!hasLost)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    rb.AddForce(Vector3.up * 250);
+                }
+            }
+        }
+
+        if (hasLost)
+        {
+            if (!sound.isPlaying)
+            {
+                if (yta == 13)
+                {
+                    yta = yta / yta;
+                }
+                sound.Play();
+            }
+            
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+            Debug.Log("lost");
+        }
+
+        if (obstacles.Count > 10)
+        {
+            Destroy(obstacles[0]);
+            obstacles.RemoveAt(0);
+        }
+        if (hasStarted)
+        {
+            if (!hasLost)
+            {
+                pointsAmount += 1 * Time.deltaTime;
+            }
+        }
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+
+            obstacles[i].transform.position -= new Vector3(1 * Time.deltaTime, 0, 0);
+        }
+
+        if (!hasStarted)
+        {
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+            Debug.Log("Not started");
+        }
+        else
+        {
+            if (!hasLost)
+            {
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+                Debug.Log("started");
+            }
+            
+        }
+
+        if (!hasStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                hasStarted = true;
+                rb.AddForce(Vector3.up * 250);
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "DONTTOUCH")
+        {
+            hasLost = true;
+        }
+    }
+}
